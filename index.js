b777b4d1ecc729000640bcd47d25ce86255bce8f
Alex <agamyan@outlook.com>
2021-03-16 13:50:17 +0400

    init

---
+++ index.js
@@ -0,0 +1,19 @@
+import {Select} from './select/select'
+import './select/styles.scss'
+
+const select = new Select('#select', {
+    placeholder: 'Please choose',
+    data: [
+        {id: '1', value: 'React'},
+        {id: '2', value: 'Angular'},
+        {id: '3', value: 'Vue'},
+        {id: '4', value: 'React Native'},
+        {id: '5', value: 'Next'},
+        {id: '6', value: 'Nest'}
+    ],
+    onSelect(item) {
+        console.log('Selected item', item);
+    }
+})
+
+window.s = select
